59817cbec19d0db68ac766e8bc4ec7e75ccacb2f
Nico Bentley <ncbentley4@gmail.com>
2020-06-18 10:56:55 -0600

    custom fields in registration

--- main_app/context_processors.py
+++ main_app/context_processors.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from .forms import CustomUserCreationForm
 
 def login_form(request):
     return {
@@ -7,5 +8,5 @@ def login_form(request):
 
 def registration_form(request):
     return {
-        'registration_form': UserCreationForm()
+        'registration_form': CustomUserCreationForm()
     }
