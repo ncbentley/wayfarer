8ab9e40bfec8812e54bae9e7f77a43bda1cdb80c
aliciaroeck16 <aliciamata00@gmail.com>
2020-06-18 21:11:45 -0500

    resolves conflict

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,11 +6,8 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('signup/', views.signup, name='signup'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
-<<<<<<< HEAD
     path('accounts/login/', views.login, name='login'),
     path('cities/', views.cities, name='cities'),
     path('cities/<int:city_id>/', views.city_index, name='city_index'),
     path('cities/<int:city_id>/<int:post_id>',views.post, name='post'),
-=======
->>>>>>> submaster
 ]
