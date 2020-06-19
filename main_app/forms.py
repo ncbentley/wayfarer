c69888fe73d9cb465aacc5e7e9d389214389b0f7
csgill <christinegill32@gmail.com>
2020-06-19 15:28:55 -0700

    edit & delete

--- main_app/forms.py
+++ main_app/forms.py
@@ -19,3 +19,8 @@ class NewPostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('city', 'title', 'content')
+
+class EditPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'content')
