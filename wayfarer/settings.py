f0349c98dee85822c437bd449987d40ca2b843c4
Nico Bentley <ncbentley4@gmail.com>
2020-06-23 16:48:11 -0600

    tryna deploy

--- wayfarer/settings.py
+++ wayfarer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,7 @@ LOGIN_REDIRECT_URL = '/profile/'
 LOGIN_URL = '/?login=true'
 MEDIA_URL = '/main_app/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'main_app/static/')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+django_heroku.settings(locals())
